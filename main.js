976c397a2f49616a81f1a5092d84e8936c5f4851
Mariakhan4621 <160171512+Mariakhan4621@users.noreply.github.com>
2024-03-14 20:04:20 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,2 @@
+var person = { name: "Maria", fname: "female", age: 18 };
+console.log(person);
---
+++ main.ts
@@ -0,0 +1,2 @@
+let  person: {name: string, fname: string , age: number} = {name:"Maria", fname: "female" , age: 18};
+console.log(person)
